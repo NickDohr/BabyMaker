a8921dff5a42649cfdc7853df50f744e8770c902
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2021-09-21 11:26:55 -0600

    Baby Maker - Intro to POJOS

---
+++ Baby Maker/src/BabyMaker.java
@@ -0,0 +1,15 @@
+
+public class BabyMaker
+	{
+
+		public static void main(String[] args)
+			{
+				Baby myB1 = new Baby("Benjamin", 7.7, false);
+				Baby myB2 = new Baby("Alfred", 9, false);
+				Baby myB3 = new Baby("Johnny", 10, false);
+				
+				System.out.println(myB1.name + " was his name!");
+				myB1.barf();
+			}
+
+	}
