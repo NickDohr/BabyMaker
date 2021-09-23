6926eee721b1818dc0600f383e9d5aca97905241
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2021-09-23 11:16:58 -0600

    Classroom Change

--- Baby Maker/src/BabyMaker.java
+++ Baby Maker/src/BabyMaker.java
@@ -8,7 +8,9 @@ public class BabyMaker
 				Baby myB2 = new Baby("Alfred", 9, false);
 				Baby myB3 = new Baby("Johnny", 10, false);
 				
-				System.out.println(myB1.name + " was his name!");
+				System.out.println(myB1.getName() + " was his name!");
+				myB1.setName("Jeffrey The Third");
+				System.out.println(myB1.getName() + " was his name!");
 				myB1.barf();
 			}
 
